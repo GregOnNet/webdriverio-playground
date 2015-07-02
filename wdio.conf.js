d91173b8fe3a5e41a472d15023dfc51e1a98c231
Gregor Woiwode <g.woiwode@cid.de>
2015-07-02 15:49:26 +0200

    Configures selenium server explicitly (needed on windows)

--- wdio.conf.js
+++ wdio.conf.js
@@ -1,5 +1,17 @@
 exports.config = {
 
+    // =====================
+    // Server Configurations
+    // =====================
+    // Host address of the running Selenium server. This information is usually obsolete as
+    // WebdriverIO automatically connects to localhost. Also if you are using one of the
+    // supported cloud services like Sauce Labs, Browserstack or Testing Bot you also don't
+    // need to define host and port information because WebdriverIO can figure that our
+    // according to your user and key information. However if you are using a private Selenium
+    // backend you should define the host address and port here.
+    //
+    host: '127.0.0.1',
+    port: 4444,
     //
     // ==================
     // Specify Test Files
